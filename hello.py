f120759e2399d1d504757f985a17d8968910b211
sebastiancamachoc <91989825+sebastiancamachoc@users.noreply.github.com>
2024-03-17 20:57:29 -0600

    first commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+print ("hola, prueba de concepto")
+
+a = {1,2}
+b = {2,3}
+print(a & b)
+
+
+original = [1, 2, 3, 4, 5]
+new = []
+
+for x in original:
+    new.append(x * 2)
+
+
+
+a = {1,2}
+b = {2,3}
+print(a - b)
+
+
+names = {'Nicolas', 'Miguel', 'Juan', 'Nicolas'} 
+print(names) 
